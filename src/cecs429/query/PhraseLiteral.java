fb5a2204f37627d19be95f82db5e9744a8a1a199
Krutika Pathak <krutikapathak084@gmail.com>
2020-10-28 22:48:05 -0700

    Phrase literal logic fix and disk get posting implementation

--- src/cecs429/query/PhraseLiteral.java
+++ src/cecs429/query/PhraseLiteral.java
@@ -1,9 +1,12 @@
 package cecs429.query;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import cecs429.index.DiskPositionalIndex;
 import cecs429.index.Index;
 import cecs429.index.Posting;
 
@@ -36,9 +39,49 @@ public class PhraseLiteral implements QueryComponent {
 	}
 
 	@Override
-	public List<Posting> getPostings(Index index) {
+	public List<Posting> getPostings(Index index, String directory) {
 		List<Posting> result = new ArrayList<Posting>();
 
+		if (mTerms.size() != 2) {
+			List<Posting> firstTerm = index.getPostingsDocandPos(mTerms.get(0), directory);
+			int i = 1;
+			do {
+				result.clear();
+				List<Posting> secondTerm = index.getPostingsDocandPos(mTerms.get(i), directory);
+				result = tempResult(firstTerm, secondTerm);
+				i++;
+				firstTerm.clear();
+				firstTerm.addAll(result);
+			} while (i < mTerms.size());
+		} else {
+			DataInputStream din;
+			DiskPositionalIndex disk = new DiskPositionalIndex();
+			try {
+				din = new DataInputStream(new FileInputStream(directory + "/index/Biwordpostings.bin"));
+				String term = mTerms.get(0) + " " + mTerms.get(1);
+				int totalDocs = disk.seekByteLoc(term, din);
+				int i = 0;
+				do {
+					int docIdGap = din.readInt();
+					int prevDocId = 0;
+					if (result.size() > 0) {
+						prevDocId = result.get(i - 1).getDocumentId();
+					}
+					int docId = docIdGap + prevDocId;
+					Posting p = new Posting(docId);
+					result.add(p);
+					i++;
+				} while (i < totalDocs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public List<Posting> getPostings(Index index) {
+		List<Posting> result = new ArrayList<Posting>();
 		List<Posting> firstTerm = index.getPostings(mTerms.get(0));
 		int i = 1;
 		do {
@@ -49,7 +92,6 @@ public class PhraseLiteral implements QueryComponent {
 			firstTerm.clear();
 			firstTerm.addAll(result);
 		} while (i < mTerms.size());
-
 		return result;
 	}
 
@@ -62,12 +104,15 @@ public class PhraseLiteral implements QueryComponent {
 			if (docListOne.get(i).getDocumentId() == docListTwo.get(j).getDocumentId()) {
 				List<Integer> positionListOne = docListOne.get(i).getmPositionId();
 				List<Integer> positionListTwo = docListTwo.get(j).getmPositionId();
-
+				List<Integer> tempPosition = new ArrayList<Integer>();
 				int a = 0, b = 0;
 				while (a < positionListOne.size() && b < positionListTwo.size()) {
 					if (positionListTwo.get(b) == positionListOne.get(a) + 1) {
-						tempResult.add(docListTwo.get(j));
-						break;
+//						tempResult.add(docListTwo.get(j));
+//						break;
+						tempPosition.add(positionListTwo.get(b));
+						a++;
+						b++;
 					} else if (positionListTwo.get(b) > positionListOne.get(a)) {
 						a++;
 					} else if (positionListTwo.get(b) < positionListOne.get(a)) {
@@ -75,9 +120,11 @@ public class PhraseLiteral implements QueryComponent {
 					} else if (positionListTwo.get(b) == positionListOne.get(a)) {
 						b++;
 					}
-
 				}
-
+				if (tempPosition.size() > 0) {
+					Posting p = new Posting(docListTwo.get(j).getDocumentId(), tempPosition);
+					tempResult.add(p);
+				}
 				i++;
 				j++;
 			} else {
@@ -91,9 +138,7 @@ public class PhraseLiteral implements QueryComponent {
 				}
 			}
 		}
-
 		return tempResult;
-
 	}
 
 	@Override
